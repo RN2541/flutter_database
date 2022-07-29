31ddd5e140ecdfcdd28fa4fa23fec0d1e2d531be
Ice <icekong0906@gmail.com>
2022-07-29 17:52:33 +0700

    1cm

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'แอพบัญชี'),
+      home: const MyHomePage(title: 'บัญชี'),
     );
   }
 }
