d091468d9fa6c02ea94fa6f0ace68d135c23f018
Ice <icekong0906@gmail.com>
2022-07-30 00:32:37 +0700

    15cm

---
+++ lib/providers/transaction_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_database/models/Transaction.dart';
+
+class TransactionProvider with ChangeNotifier {
+  // ตัวอย่างข้อมูล
+  List<Transaction> transaction = [
+    Transaction(title: "ซื้อหนังสือ", amount: 500, date: DateTime.now()),
+    Transaction(title: "เสื้อผ้า", amount: 900, date: DateTime.now()),
+    Transaction(title: "กางเกง", amount: 400, date: DateTime.now())
+  ];
+
+  // ดึงข้อมูล
+  List<Transaction> getTransaction() {
+    return transaction;
+  }
+
+  void addTransaction(Transaction statement) {
+    transaction.add(statement);
+  }
+}
